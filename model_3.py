e6e94c81db2c06bb25f9e2a98e653a77c2a4a9e2
karabas94 <alkar_94@ukr.net>
2024-04-18 23:49:29 +0300

    innit commit

--- model_3.py
+++ model_3.py
@@ -6,27 +6,21 @@ from matplotlib import pyplot as plt
 
 df = pd.read_csv('1h_last3.csv')
 
-print('\n', df)
+titles = ['Open', 'High', 'Low', 'Close']
 
-print('\n', df.info())
-
-print('\n', df.describe())
-
-titles = ['Open', 'High', 'Low', 'Close', 'Volume']
-
-feature_keys = ['Open', 'High', 'Low', 'Last', 'Volume']
+feature_keys = ['Open', 'High', 'Low', 'Last']
 
 date_time_key = "Time"
 
 split_fraction = 0.715
 train_split = int(split_fraction * int(df.shape[0]))
-step = 6
+step = 2
 
-past = 720
+past = 1000
 future = 72
 learning_rate = 0.001
-batch_size = 256
-epochs = 5
+batch_size = 128
+epochs = 1
 
 
 def normalize(data, train_split):
@@ -37,31 +31,27 @@ def normalize(data, train_split):
 
 print(
     'The selected parameters are:',
-    ', '.join([titles[i] for i in [0, 1, 2, 3, 4]])
+    ', '.join([titles[i] for i in [0, 1, 2, 3]])
 )
-selected_features = [feature_keys[i] for i in [0, 1, 2, 3, 4]]
+selected_features = [feature_keys[i] for i in [0, 1, 2, 3]]
 features = df[selected_features]
 features.index = df[date_time_key]
-print(features.head(), '\n')
 
 features = normalize(features.values, train_split)
 features = pd.DataFrame(features)
-print(features.head(), '\n')
+# print('features', features)
 
 train_data = features.loc[0:train_split - 1]
 val_data = features.loc[train_split:]
 
-print('train data: ', train_data)
-print('val_data: ', val_data)
-
 start = past + future
 end = start + train_split
 
-x_train = train_data[[i for i in range(5)]].values
+x_train = train_data[[i for i in range(4)]].values
 y_train = features.iloc[start:end][[0, 1, 2, 3]]
 
 sequence_length = int(past / step)
-print(sequence_length)
+print('sequence_length', sequence_length)
 
 dataset_train = keras.preprocessing.timeseries_dataset_from_array(
     x_train,
@@ -75,10 +65,12 @@ for batch in dataset_train.take(1):
     inputs, targets = batch
 
 x_end = len(val_data) - past - future
+print('x_end: ', x_end)
 
 label_start = train_split + past + future
+print('label_start: ', label_start)
 
-x_val = val_data.iloc[:x_end][[i for i in range(5)]].values
+x_val = val_data.iloc[:x_end][[i for i in range(4)]].values
 y_val = features.iloc[label_start:][[0, 1, 2, 3]]
 
 dataset_val = keras.preprocessing.timeseries_dataset_from_array(
@@ -99,7 +91,7 @@ print((inputs.shape[1], inputs.shape[2]))
 
 inputs = keras.layers.Input(shape=(inputs.shape[1], inputs.shape[2]))
 # lstm_out = keras.layers.SimpleRNN(32)(inputs)
-lstm_out = keras.layers.LSTM(32)(inputs)
+lstm_out = keras.layers.LSTM(64)(inputs)
 # lstm_out = keras.layers.Bidirectional(keras.layers.LSTM(32, return_sequences=False))(inputs)
 
 # temp = keras.layers.Bidirectional(keras.layers.LSTM(32, return_sequences=True))(inputs)
@@ -112,11 +104,12 @@ model.compile(optimizer=keras.optimizers.Adam(learning_rate=learning_rate), loss
 model.summary()
 
 path_checkpoint = "model_checkpoint.weights.h5"
+path_checkpointt = "modell_checkpoint.weights.h5"
 es_callback = keras.callbacks.EarlyStopping(monitor="val_loss", min_delta=0, patience=5)
 
 modelckpt_callback = keras.callbacks.ModelCheckpoint(
     monitor="val_loss",
-    filepath=path_checkpoint,
+    filepath=path_checkpointt,
     verbose=1,
     save_weights_only=True,
     save_best_only=True,
@@ -140,32 +133,73 @@ print("Shape of predictions:", predictions.shape)
 print("Predictions:", predictions)
 
 
-def denormalize(norm_data, data_mean, data_std):
-    return (norm_data * data_std) + data_mean
+def denormalize_predictions(predictions, features, train_split):
+    data_mean = features.iloc[:train_split].mean()
+    data_std = features.iloc[:train_split].std()
+    return predictions * data_std.values + data_mean.values
 
 
-# Среднее и стандартное отклонение тренировочных данных
-data_mean = train_data.mean(axis=0).values  # Получаем значения как numpy массив
-data_std = train_data.std(axis=0).values    # Получаем значения как numpy массив
+predictions_denorm = denormalize_predictions(predictions, df[selected_features], train_split)
+print("Shape of denormalize predictions:", predictions_denorm.shape)
+print("denormalize Predictions:", predictions_denorm)
 
-# Применение обратной нормализации к предсказаниям
-original_predictions = denormalize(predictions, data_mean[:4], data_std[:4])
+df_candles = pd.DataFrame(predictions_denorm, columns=['Open', 'High', 'Low', 'Close'])
 
-print("Original scale predictions:", original_predictions)
+df_candles.index = pd.date_range(start='17-08-2023 14:00', periods=len(df_candles), freq='h')
 
+print(df_candles.head())
 
-# # creating plot for last 3880 samples
-# df['Time'] = pd.to_datetime(df['Time'])
-#
-# last_3880_df = df.tail(24)
-#
-# ohlc_df = last_3880_df[['Time', 'Open', 'High', 'Low', 'Last']].copy()
-#
-# ohlc_df.columns = ['Date', 'Open', 'High', 'Low', 'Close']
-#
-# ohlc_df.set_index('Date', inplace=True)
-#
-# mpf.plot(ohlc_df, type='candle', datetime_format='%m/%d/%Y %H:%M',
-#          title='true', ylabel='price', style='charles')
-#
-# mpf.show()
+mpf.plot(df_candles, type='candle', style='charles',
+         title='predict',
+         ylabel='price')
+
+# creating plot for last 3880 samples
+df['Time'] = pd.to_datetime(df['Time'])
+
+last_3880_df = df.tail(5386)
+
+ohlc_df = last_3880_df[['Time', 'Open', 'High', 'Low', 'Last']].copy()
+
+ohlc_df.columns = ['Date', 'Open', 'High', 'Low', 'Close']
+
+ohlc_df.set_index('Date', inplace=True)
+
+mpf.plot(ohlc_df, type='candle', datetime_format='%m/%d/%Y %H:%M',
+         title='true', ylabel='price', style='charles')
+
+mpf.show()
+
+buffer = features.iloc[-sequence_length:].values
+# print('buffer', buffer)
+predictions = []
+
+for i in range(24):
+    input_buffer = buffer.reshape(1, sequence_length, 4)
+
+    pred = model.predict(input_buffer)
+
+    predictions.append(pred[0])
+
+    buffer = np.vstack([buffer[1:], pred[0]])
+    # print(i, buffer)
+
+predictions = np.array(predictions)
+# print(predictions)
+
+predictions_df = pd.DataFrame(predictions, columns=['Open', 'High', 'Low', 'Close'])
+# print("Predictions for 24h: ", predictions_df)
+
+predictions_denorm = denormalize_predictions(predictions_df, df[selected_features], train_split)
+
+print("predictions denorm for 24h: ", predictions_denorm)
+
+dates = pd.date_range(start="2024-04-02", periods=24, freq='h')
+predictions_denorm.index = dates
+
+mpf.plot(predictions_denorm[['Open', 'High', 'Low', 'Close']],
+         type='candle', style='charles',
+         title='predicted next 24h',
+         ylabel='price')
+
+plt.show()
+
